19ae68376fa19d845804be37d0337007942b1300
FurWaz <fur.waz06@gmail.com>
2023-08-08 17:34:16 +0200

    Fixed stream response when creating it

--- src/controllers/stream.ts
+++ src/controllers/stream.ts
@@ -1,34 +1,18 @@
 import type express from 'express';
 import * as sanitizer from '../tools/sanitizer';
 import { Log, ErrLog, ResLog } from '../tools/log';
-import ytdl from 'ytdl-core';
 import fs from 'fs';
+import { getOrCreateStream } from '../data/stream';
 // import * as url from 'url';
-
-// const __filename = url.fileURLToPath(import.meta.url);
-// const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 export async function makeStream (req: express.Request, res: express.Response) {
     const id = sanitizer.sanitizeStringField(req.query.id, req, res);
     if (id === null) return;
 
-    const filepath = `./tmp/${id}.mp3`;
-    if (fs.existsSync(filepath)) {
+    try {
+        await getOrCreateStream(id);
         new ResLog(res.locals.lang.info.stream.created, '/stream?id=' + id, Log.CODE.OK).sendTo(res);
         return;
-    }
-
-    try {
-        const infos = await ytdl.getInfo(id);
-        const formats = ytdl.filterFormats(infos.formats, 'audioonly');
-        const format = ytdl.chooseFormat(formats, { quality: 'lowestaudio' });
-
-        const file = fs.createWriteStream(filepath);
-        const stream = ytdl.downloadFromInfo(infos, { format });
-        stream.pipe(file);
-        stream.on('end', () => {
-            new ResLog(res.locals.lang.info.stream.created, '/stream?id=' + id, Log.CODE.OK).sendTo(res);
-        });
     } catch (err) {
         console.error(err);
         new ErrLog(res.locals.lang.error.generic.internalError, ErrLog.CODE.INTERNAL_SERVER_ERROR).sendTo(res);
@@ -39,29 +23,15 @@ export async function getStream (req: express.Request, res: express.Response) {
     const id = sanitizer.sanitizeStringField(req.query.id, req, res);
     if (id === null) return;
 
-    fs.stat(`./tmp/${id}.mp3`, (err, stats) => {
-        if (err !== null) {
-            makeStream(req, res)
-                .then(() => {})
-                .catch(() => {});
-            return;
-        }
-        if (!stats.isFile()) {
-            new ErrLog(res.locals.lang.error.stream.notFound, ErrLog.CODE.NOT_FOUND).sendTo(res);
-            return;
-        }
-
-        try {
-            const filepath = `./tmp/${id}.mp3`;
-            const stream = fs.createReadStream(filepath);
-            res.header('Content-Type', 'audio/mpeg');
-            res.header('Content-Length', stats.size.toString());
-            res.header('Content-Range', `bytes 0-${stats.size - 1}/${stats.size}`);
-            res.header('Accept-Ranges', 'bytes');
-            stream.pipe(res);
-        } catch (e) {
-            console.error(e);
-            new ErrLog(res.locals.lang.error.generic.internalError, ErrLog.CODE.INTERNAL_SERVER_ERROR).sendTo(res);
-        }
-    });
+    try {
+        const infos = await getOrCreateStream(id);
+        const stream = fs.createReadStream(infos.filepath);
+        res.header('Content-Type', 'audio/mpeg');
+        res.header('Content-Length', infos.size.toString());
+        res.header('Content-Range', `bytes 0-${infos.size - 1}/${infos.size}`);
+        res.header('Accept-Ranges', 'bytes');
+        stream.pipe(res);
+    } catch {
+        new ErrLog(res.locals.lang.error.stream.notFound, ErrLog.CODE.NOT_FOUND).sendTo(res);
+    }
 }
---
+++ src/data/stream.ts
@@ -0,0 +1,38 @@
+
+import ytdl from 'ytdl-core';
+import fs from 'fs';
+
+interface StreamResult {
+    filepath: string
+    size: number
+}
+
+export async function getOrCreateStream (id: string): Promise<StreamResult> {
+    return new Promise((resolve, reject) => {
+        const filepath = `./tmp/${id}.mp3`;
+        fs.stat(filepath, (err, stats) => {
+            if (err !== null) {
+                ytdl.getInfo(id).then(infos => {
+                    const formats = ytdl.filterFormats(infos.formats, 'audioonly');
+                    const format = ytdl.chooseFormat(formats, { quality: 'lowestaudio' });
+
+                    const file = fs.createWriteStream(filepath);
+                    const stream = ytdl.downloadFromInfo(infos, { format });
+                    stream.pipe(file);
+                    stream.on('end', () => {
+                        resolve({ filepath, size: stats.size });
+                    });
+                }).catch(err => {
+                    reject(err);
+                });
+                return;
+            }
+            if (!stats.isFile()) {
+                reject(new Error('not found'));
+                return;
+            }
+
+            resolve({ filepath, size: stats.size });
+        });
+    });
+}
